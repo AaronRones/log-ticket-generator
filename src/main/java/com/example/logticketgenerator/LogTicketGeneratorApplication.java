f33ef565891debe33a62f183a680ee28977c9793
aaron.rones <aaron.rones@hudsonmx.com>
2023-08-10 16:28:53 -0400

    initialized project with spring initializer
    Signed-off-by: aaron.rones <aaron.rones@hudsonmx.com>

---
+++ src/main/java/com/example/logticketgenerator/LogTicketGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.logticketgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTicketGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTicketGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logticketgenerator/LogTicketGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logticketgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTicketGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
